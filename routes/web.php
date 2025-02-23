1c49ac1e82f05744dd36c5591d0995b5d2e15f33
shakhaowat-alam <mdshakhaowat214@gmail.com>
2025-02-22 16:37:27 -0800

    file added successfully

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+route::get('/redirect',[HomeController::class,'redirect']);
+
+route::get('/',[HomeController::class,'index']);
